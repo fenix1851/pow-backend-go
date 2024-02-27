5e0a715e94a4a1a6ae4a6d5b4c1f00a9cfa5764c
fenix1851 <dmpytyi@gmail.com>
2024-02-27 02:31:58 +0100

    Update Docker Compose file and add startWithDelayInDocker.sh script

---
+++ quotes-client/startWithDelayInDocker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+while ! nc -z quotes-server 8080; do
+echo 'Waiting for the quotes-server to come online...'
+sleep 1
+done
+echo 'Quotes-server is up and running!'
+go run main.go
+-baseUrl http://quotes-server:8080
