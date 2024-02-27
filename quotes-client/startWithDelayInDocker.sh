f0bfbb048c5c3c148dbf19baa5698a915d9d0510
fenix1851 <dmpytyi@gmail.com>
2024-02-27 02:39:59 +0100

    Update Dockerfile and start script to use quotes-server with a base URL

--- quotes-client/startWithDelayInDocker.sh
+++ quotes-client/startWithDelayInDocker.sh
@@ -5,5 +5,4 @@ echo 'Waiting for the quotes-server to come online...'
 sleep 1
 done
 echo 'Quotes-server is up and running!'
-go run main.go
--baseUrl http://quotes-server:8080
+go run main.go --baseUrl http://quotes-server:8080
